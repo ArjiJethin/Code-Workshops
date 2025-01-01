534ea605c1b7966579f7e971d480d35b30fcb694
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-01-01 09:55:20 +0530

    Add Problem10.java to Conditional0 package

---
+++ Conditional0/Problem10.java
@@ -0,0 +1,18 @@
+package Talentely.Conditional0;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.next().charAt(0);
+        scanner.close();
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
+                letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+
+}
