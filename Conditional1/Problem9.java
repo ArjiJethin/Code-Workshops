75f55e85deb2125b72e8ac69af142e38d9effe9a
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-01-01 09:56:16 +0530

    Add Problem9.java to Conditional1 package

---
+++ Conditional1/Problem9.java
@@ -0,0 +1,30 @@
+package Talentely.Conditional1;
+
+import java.util.Scanner;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        char op = scanner.next().charAt(0);
+        int num2 = scanner.nextInt();
+        scanner.close();
+        int ans = 0;
+        switch (op) {
+            case '+':
+                ans = num1 + num2;
+                break;
+            case '-':
+                ans = num1 - num2;
+                break;
+            case '*':
+                ans = num1 * num2;
+                break;
+            case '/':
+                ans = num1 / num2;
+                break;
+        }
+        System.out.println(ans);
+    }
+
+}
