af34816e3cc4b02974d47537f7601ed75f01ef3a
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-01-01 10:43:17 +0530

    Add C code for Problem 1: Input and Output Operations in C

---
+++ C/InOp2/Problem1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    long long int N1, N2;
+    scanf(" % lld % lld ", &N1, &N2);
+    printf(" Sum of % lld and % lld is % lld ", N1, N2, N1 + N2);
+    return 0;
+}
