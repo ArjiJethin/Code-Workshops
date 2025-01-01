094294ae7ebb701071df488a8f712619a15a8743
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-01-01 09:55:09 +0530

    Add Problem8.java to Conditional0 package

---
+++ Conditional0/Problem8.java
@@ -0,0 +1,21 @@
+package Talentely.Conditional0;
+
+import java.util.Scanner;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long num = scanner.nextLong();
+        scanner.close();
+        if (num % 3 == 0 && num % 5 == 0) {
+            System.out.println("HIHELLO");
+        } else if (num % 3 == 0) {
+            System.out.println("HI");
+        } else if (num % 5 == 0) {
+            System.out.println("HELLO");
+        } else {
+            System.out.println("NONE");
+        }
+    }
+
+}
