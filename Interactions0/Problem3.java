2ab885fe12d2cc0c8d544cac0fe77ece90cabb7c
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-01-01 09:58:51 +0530

    Add Problem3.java to Interactions0 package

---
+++ Interactions0/Problem3.java
@@ -0,0 +1,8 @@
+
+public class Problem3 {
+    public static void main(String[] args) {
+        System.out.println("Success in when your \"Signature\" becomes \"autograph\"");
+    }
+}
+
+// 2 Marks
