277f1fd212a01366883b3430fb5fd309be8b5c1e
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-01-04 14:59:28 +0530

    Refactor package structure for Problem4.java

---
+++ JAVA/Conditional0/Problem4.java
@@ -0,0 +1,16 @@
+package Conditional0;
+
+import java.util.Scanner;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mark = scanner.nextInt();
+        scanner.close();
+        if (mark >= 35) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+}
